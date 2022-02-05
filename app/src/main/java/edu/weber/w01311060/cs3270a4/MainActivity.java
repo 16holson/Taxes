942ddae3d71b87d2c8a027926ef06f36dc273ed9
Hunter Olson <hunterolson@mail.weber.edu>
2022-02-05 10:37:10 -0700

    Initial Commit

---
+++ app/src/main/java/edu/weber/w01311060/cs3270a4/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.weber.w01311060.cs3270a4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
